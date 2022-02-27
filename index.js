4fe184eac5b06a47f44f4c14af9bea50fd024dcc
jrwj97 <jrwj97@aol.com>
2022-02-27 00:01:31 -0500

    created catch function for each question to require an answer

--- index.js
+++ index.js
@@ -1,61 +1,136 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer
-  .prompt([
+const generateMarkdown = require(`./utils/generateMarkdown.js`);
+
+const questions = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "githubName",
-      message: "What is your GitHub username?"
+      message: "What is your GitHub username?",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "userEmail",
       message: "What is your user email?",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "projectTitle",
       message:"What is the name of your project?",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "projectDescription",
       message:"Give a description of your project.",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "projectInstall",
       message:"What are the directions to install you project?",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "checkbox",
       name: "projectPrograms",
       message:"What programs did you use to create your project?",
-      choices: ["HTML", "JavaScript", "CSS"]
+      choices: ["HTML", "JavaScript", "CSS"],
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "projectContributors",
       message:"Who are the authors of your project?",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     },
     {
       type: "input",
       name: "projectGuide",
       message: "Give a tutorial on how to use your project",
+      validate: input => {
+        if (input) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false; 
+        }
+      }
     }
   ])
-  .then(answers => console.log(answers));
-// // TODO: Include packages needed for this application
+}
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
+const writeFile = data => {
+  fs.writeFile("README.md", data, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Your README has been created succesfully!")
+    }
+  })
+};
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+questions()
+.then(answers => {
+  return generateMarkdown(answers);
+})
+.then (data => {
+  return writeFile(data);
+})
+.catch(err => {
+  console.log(err)
+})
